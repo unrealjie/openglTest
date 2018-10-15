b07b8831fe9ed25724146daf50b3fced0dd0c899
unrealjie <factorialunderstander@hotmail.com>
2018-10-16 00:57:34 +0900

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,10 @@
 #include<wglew.h>
 #include<GL/GL.h>
 #include<windows.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
 
 static TCHAR szClassName[] = TEXT("CLEAR");
 static TCHAR szWindowName[] = TEXT("Clear");
@@ -10,10 +14,82 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT nMessage, WPARAM wParam, LPARAM lPar
 static void OnCreate(HWND hWnd);
 static void OnPaint(HWND hWnd);
 static void OnDestroy(HWND hWnd);
+static void OnSize(HWND hWnd, int nWidth, int nHeight);
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT nMessage, WPARAM wParam, LPARAM lParam);
 
 HGLRC g_hGLRC;
+GLuint g_vertexShader;
+GLuint g_fragShader;
+GLuint g_shaderProgram;
+GLuint g_bufferObject;
+GLuint g_vertexArrayObject;
+
+
+static void LoadShaderSource(GLuint shader,const TCHAR* fileName) 
+{
+
+	FILE* fp = NULL;
+	int size;
+	char* buf;
+
+	fp = fopen(fileName,"rb");
+	fseek(fp,0,SEEK_END);
+	size = ftell(fp);
+
+	buf = (char*)malloc(size);
+	fseek(fp,0,SEEK_SET);
+	fread(buf,1,size,fp);
+
+	glShaderSource(shader,1,(const GLchar**)&buf,&size);
+	free(buf);
+	fclose(fp);
+
+}
+
+static void DisplayCompileError(GLuint shader, HWND hWnd) 
+{
+	GLint compiled;
+	int size;
+	int len;
+	char*buf;
+
+	glGetShaderiv(shader,GL_COMPILE_STATUS,&compiled);
+	if (compiled == GL_FALSE) 
+	{
+		glGetShaderiv(shader,GL_INFO_LOG_LENGTH,&size);
+		if (size > 0) 
+		{
+			buf = (char*)malloc(size);
+			glGetShaderInfoLog(shader,size,&len,buf);
+			MessageBoxA(hWnd,buf,"Compile error !",MB_OK);
+			free(buf);
+		}
+	}
+	return;
+}
+
+static void DisplayLinkError(GLuint program,HWND hWnd) 
+{
+	GLint linked;
+	int size;
+	int len;
+	char* buf;
+
+	glGetProgramiv(program,GL_LINK_STATUS,&linked);
+	if (linked == GL_FALSE) {
+		glGetProgramiv(program,GL_INFO_LOG_LENGTH,&size);
+		if (size > 0) 
+		{
+			buf = (char*)malloc(size);
+			glGetProgramInfoLog(program,size,&len,buf);
+			MessageBoxA(hWnd, buf, "Link error !", MB_OK);
+			free(buf);
+		}
+	}
+
+	return;
+}
 
 int WINAPI WinMain(HINSTANCE hCurrInstance,HINSTANCE hPrevInstance,LPSTR szArgs,int nWinMode) {
 	HWND hWnd;
@@ -94,6 +170,13 @@ static void OnCreate(HWND hWnd)
 	BOOL bResult;
 	HGLRC hGLRC;
 
+	const GLfloat colorsAndVertices[3][5] = {
+		{1.0f,1.0f,0.0f,-0.5f,-0.5f},
+		{1.0f, 1.0f, 0.0f, 0.5f, -0.5f},
+		{1.0f,1.0f,0.0f,0.0f,0.5f},
+	};
+
+
 	const PIXELFORMATDESCRIPTOR pfd = {
 		sizeof(PIXELFORMATDESCRIPTOR),
 		1,
@@ -114,7 +197,7 @@ static void OnCreate(HWND hWnd)
 		0
 	};
 	const int attributes[] = {
-		WGL_CONTEXT_MAJOR_VERSION_ARB,4,
+		WGL_CONTEXT_MAJOR_VERSION_ARB,3,
 		WGL_CONTEXT_MINOR_VERSION_ARB,0,
 		WGL_CONTEXT_PROFILE_MASK_ARB,
 		WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
@@ -144,14 +227,71 @@ static void OnCreate(HWND hWnd)
 	wglMakeCurrent(NULL,NULL);
 	wglDeleteContext(g_hGLRC);
 	g_hGLRC = hGLRC;
+
+	wglMakeCurrent(hDC,g_hGLRC);
+
+	g_vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	LoadShaderSource(g_vertexShader,"c:\\shader\\Simple.vert");
+	glCompileShader(g_vertexShader);
+	DisplayCompileError(g_vertexShader,hWnd);
+
+	g_fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	LoadShaderSource(g_fragShader,"c:\\shader\\Frag.vert");
+	glCompileShader(g_fragShader);
+	DisplayCompileError(g_fragShader,hWnd);
+
+	g_shaderProgram = glCreateProgram();
+	glAttachShader(g_shaderProgram,g_vertexShader);
+	glAttachShader(g_shaderProgram,g_fragShader);
+
+	glDeleteShader(g_vertexShader);
+	glDeleteShader(g_fragShader);
+	glLinkProgram(g_shaderProgram);
+	DisplayLinkError(g_shaderProgram,hWnd);
+
+	glGenBuffers(1,&g_bufferObject);
+	glBindBuffer(GL_ARRAY_BUFFER,g_bufferObject);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(colorsAndVertices),colorsAndVertices,GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER,0);
+
+	GLint colorLocation = glGetAttribLocation(g_shaderProgram, "Color");
+	GLint vertexLocation = glGetAttribLocation(g_shaderProgram,"Vertex");
+
+	glGenVertexArrays(1,&g_vertexArrayObject);
+	glBindVertexArray(g_vertexArrayObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER,g_bufferObject);
+	glVertexAttribPointer(colorLocation,3,GL_FLOAT,GL_FALSE,5*sizeof(GLfloat),0);
+	glVertexAttribPointer(vertexLocation, 2, GL_FLOAT, GL_FALSE,5*sizeof(GLfloat),(GLvoid *)(3*sizeof(GLfloat)));
+	glEnableVertexAttribArray(colorLocation);
+	glEnableVertexAttribArray(vertexLocation);
+	glBindBuffer(GL_ARRAY_BUFFER,0);
+	glBindVertexArray(0);
+
+	glDisableVertexAttribArray(glGetAttribLocation(g_shaderProgram,"Color"));
+	glDisableVertexAttribArray(glGetAttribLocation(g_shaderProgram,"Vertex"));
+
+	glEnable(GL_CULL_FACE);
+	glClearColor(0.0f,0.0f,0.2f,1.0f);
+
+	wglMakeCurrent(NULL, NULL);
+
 	ReleaseDC(hWnd,hDC);
 }
 
 static void OnDestroy(HWND hWnd) 
 {
+	HDC hDC;
+	hDC = GetDC(hWnd);
+	wglMakeCurrent(hDC,g_hGLRC);
+	glDeleteVertexArrays(1,&g_vertexArrayObject);
+	glDeleteBuffers(1,&g_bufferObject);
+	glDeleteProgram(g_shaderProgram);
+	wglMakeCurrent(NULL,NULL);
 	if (g_hGLRC != NULL) {
 		wglDeleteContext(g_hGLRC);
 	}
+	ReleaseDC(hWnd,hDC);
 }
 
 static void OnPaint(HWND hWnd) {
@@ -163,9 +303,30 @@ static void OnPaint(HWND hWnd) {
 	wglMakeCurrent(hDC,g_hGLRC);
 	glClearColor(0.0f,0.0f,0.5f,1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(g_shaderProgram);
+	glBindVertexArray(g_vertexArrayObject);
+	glDrawArrays(GL_TRIANGLES,0,3);
+	glBindVertexArray(0);
+	glFlush();
+	glUseProgram(0);
+
+
 	SwapBuffers(hDC);
 	wglMakeCurrent(NULL,NULL);
 
 	EndPaint(hWnd,&ps);
 
 }
+
+static void OnSize(HWND hWnd, int nWidth, int nHeight) 
+{
+	HDC hDC;
+	hDC = GetDC(hWnd);
+	wglMakeCurrent(hDC, g_hGLRC);
+	glViewport(0,0,nWidth,nHeight);
+	wglMakeCurrent(NULL,NULL);
+	ReleaseDC(hWnd,hDC);
+	InvalidateRect(hWnd,NULL,NULL);
+}
+
